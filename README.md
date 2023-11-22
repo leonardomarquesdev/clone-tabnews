57f212e58c181c3d373ccea11424fabe42843f0b
LeonardoMarques <leonardomarques.dev@gmail.com>
2023-11-22 11:53:13 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Implementação do https://tabnews.com.br para alcançar outro nível na carreira. 
+
+Implementação do https://tabnews.com.br para alcançar outro nível na carreira.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Vanessa, eu amo você! Se você me ama, dê uma risadinha. =D </h1>
+  return <h1>Vanessa, eu amo você! Se você me ama, dê uma risadinha. =D </h1>;
 }
 
 export default Home;
